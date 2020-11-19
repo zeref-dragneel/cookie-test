cf494ba04cb9c16dfcaa63dc593c32a2dd25aba1
zeref-dragneel <sharathdesignsofficial@gmail.com>
2020-11-19 21:29:54 +0530

    new point added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.use(cors())
 app.get("/",(req,res)=>{
     res.status(200).cookie("auth","authorizer",{httpOnly:true}).json({'hello':"hello"})
 })
+app.get("/app/test",(req,res)=>{
+    res.status(200).cookie("auth","authorizer",{httpOnly:true}).json({'hello':"test hello"})
+})
 app.listen(process.env.PORT || 3000,()=>{
     console.log('server running on port 3000')
 })
