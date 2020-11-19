65f11edfaaa5d071c128f1738b514a1709391c52
zeref-dragneel <sharathdesignsofficial@gmail.com>
2020-11-19 21:19:06 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+var cors = require('cors')
+app.use(cors())
 app.get("/",(req,res)=>{
     res.status(200).cookie("auth","authorizer",{httpOnly:true}).send("hello world")
 })
