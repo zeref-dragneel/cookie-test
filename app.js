aa8ac71cfaa26c4815e7ed2d70ebb14e25fe7ea6
zeref-dragneel <sharathdesignsofficial@gmail.com>
2020-11-19 21:01:47 +0530

    provess env added

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 app.get("/",(req,res)=>{
     res.status(200).cookie("auth","authorizer",{httpOnly:true}).send("hello world")
 })
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log('server running on port 3000')
 })
