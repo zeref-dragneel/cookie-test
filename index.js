a603a761164b07833c23227a6a416311a7db221e
zeref-dragneel <sharathdesignsofficial@gmail.com>
2020-11-19 21:23:35 +0530

    json loaded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 var cors = require('cors')
 app.use(cors())
 app.get("/",(req,res)=>{
-    res.status(200).cookie("auth","authorizer",{httpOnly:true}).send("hello world")
+    res.status(200).cookie("auth","authorizer",{httpOnly:true}).json({'hello':"hello"})
 })
 app.listen(process.env.PORT || 3000,()=>{
     console.log('server running on port 3000')
